ccbf55e12bc4bfd451ca75beeb5f9fa56544970e
svenlc <sven.lecann@live.fr>
2019-01-03 23:18:15 +0100

    Change axios.get url

--- controllers/offreCasting.js
+++ controllers/offreCasting.js
@@ -1,11 +1,9 @@
 const axios = require('axios');
 
 exports.getOffreCastings = (req, res, next) => {
-    axios.get('http://localhost:3000/offreCastings/formated')
-    .then(offres => {
-        console.log(offres.data);
-        res.status(200).json({offres: offres.data});
-        
+    axios.get('https://megacastingprivateapi.azurewebsites.net/offreCastings/formated')
+    .then(offres => {        
+        res.status(200).json({offres: offres.data});       
         
     })
     .catch(err => {
