8e910469e87ed8391d8baa3f21ca9cd2f4fd7ca5
svenl <sven.lecann@live.fr>
2019-01-08 03:38:27 +0100

    Auth logic issue

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -2,28 +2,33 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
     const autHeader = req.get('Authorization');
-    if (req.headers.host == megacastingwebsite.herokuapp.com) {
+    const domain = 'megacastingwebsite.herokuapp.com'
+    if (req.headers.host == domain) {
+        console.log('Auth ok !');
+        next();
+    }
+    else {
+        if (!autHeader) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = autHeader.split(' ')[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
+        } catch (error) {
+            error.statusCode = 500;
+            throw error;
+        }
+        if (!decodedToken) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        req.userId = decodedToken.userId;
         next();
     } 
-    if (!autHeader) {
-        const error = new Error('Non authentifié');
-        error.statusCode = 401;
-        throw error;
-    }
-    const token = autHeader.split(' ')[1];
-    let decodedToken;
-    try {
-        decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
-    } catch (error) {
-        error.statusCode = 500;
-        throw error;
-    }
-    if (!decodedToken) {
-        const error = new Error('Non authentifié');
-        error.statusCode = 401;
-        throw error;
-    }
-    req.userId = decodedToken.userId;
-    next();
+    
 };
 
