78fc8aee5a6d11364dfcc7cfca61252803334f0e
svenl <sven.lecann@live.fr>
2019-01-08 05:59:08 +0100

    auth test

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -11,41 +11,34 @@ module.exports = (req, res, next) => {
     dns.lookup('megacastingwebsite.herokuapp.com', (err, adresse) => {
         ipWebSite = adresse;
         res.status(200).json({ipWP: ipWP, ipWebSite: ipWebSite});
-        // .split(':')[0]
-        // Prints: localhost ssh
-    })
-    
         
-   
-
+    }) 
     
-    
-    // if (req.hostname == domain) {
-    //     console.log('Auth ok !');
-    //     next();
-    // }
-    // else {
-    //     if (!autHeader) {
-    //         const error = new Error('Non authentifié');
-    //         error.statusCode = 401;
-    //         throw error;
-    //     }
-    //     const token = autHeader.split(' ')[1];
-    //     let decodedToken;
-    //     try {
-    //         decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
-    //     } catch (error) {
-    //         error.statusCode = 500;
-    //         throw error;
-    //     }
-    //     if (!decodedToken) {
-    //         const error = new Error('Non authentifié');
-    //         error.statusCode = 401;
-    //         throw error;
-    //     }
-    //     req.userId = decodedToken.userId;
-    //     next();
-    // } 
+    if (ipWP == ipWebSite) {        
+        next();
+    }
+    else {
+        if (!autHeader) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = autHeader.split(' ')[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
+        } catch (error) {
+            error.statusCode = 500;
+            throw error;
+        }
+        if (!decodedToken) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        req.userId = decodedToken.userId;
+        next();
+    } 
     
 };
 
