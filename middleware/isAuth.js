7bbcc90ed2a6b52e62d5eefac2166b636004682e
svenl <sven.lecann@live.fr>
2019-01-08 06:53:12 +0100

    auth test

--- middleware/isAuth.js
+++ middleware/isAuth.js
@@ -9,31 +9,31 @@ module.exports = (req, res, next) => {
     if (ip == domain) {        
         next();
     }
-    // else {
+    else {
 
-    //     const autHeader = req.get('Authorization');
+        const autHeader = req.get('Authorization');
 
-    //     if (!autHeader) {
-    //         const error = new Error('Non authentifié');
-    //         error.statusCode = 401;
-    //         throw error;
-    //     }
-    //     const token = autHeader.split(' ')[1];
-    //     let decodedToken;
-    //     try {
-    //         decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
-    //     } catch (error) {
-    //         error.statusCode = 500;
-    //         throw error;
-    //     }
-    //     if (!decodedToken) {
-    //         const error = new Error('Non authentifié');
-    //         error.statusCode = 401;
-    //         throw error;
-    //     }
-    //     req.userId = decodedToken.userId;
-    //     next();
-    // } 
+        if (!autHeader) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        const token = autHeader.split(' ')[1];
+        let decodedToken;
+        try {
+            decodedToken = jwt.verify(token, 'BDB971EA6E6788317F359F23E86C5');
+        } catch (error) {
+            error.statusCode = 500;
+            throw error;
+        }
+        if (!decodedToken) {
+            const error = new Error('Non authentifié');
+            error.statusCode = 401;
+            throw error;
+        }
+        req.userId = decodedToken.userId;
+        next();
+    } 
     
 };
 
